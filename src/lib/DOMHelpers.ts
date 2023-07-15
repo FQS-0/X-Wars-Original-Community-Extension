23d29950059ea028c45bbeddc7d05a3e33430d16
FQS <oliver@riesewohld.net>
2023-07-15 19:04:51 +0200

    converted server time from UTC to local time

---
+++ src/lib/DOMHelpers.ts
@@ -0,0 +1,26 @@
+export class DOMHelpers {
+    static asHTMLElement(element: Element | null | undefined): HTMLElement {
+        if (!element) throw "Error: element is null or undefined!"
+        if (!(element instanceof HTMLElement))
+            throw "Error: element is no HTMLElement"
+        return element
+    }
+
+    static asHTMLSelectElement(
+        element: Element | RadioNodeList | null | undefined
+    ): HTMLSelectElement {
+        if (!element) throw "Error: element is null or undefined!"
+        if (!(element instanceof HTMLSelectElement))
+            throw "Error: element is no HTMLSelectElement"
+        return element
+    }
+
+    static asHTMLFormElement(
+        element: Element | RadioNodeList | null | undefined
+    ): HTMLFormElement {
+        if (!element) throw "Error: element is null or undefined!"
+        if (!(element instanceof HTMLFormElement))
+            throw "Error: element is no HTMLFormElement"
+        return element
+    }
+}
--- src/scripts/content/status.ts
+++ src/scripts/content/status.ts
@@ -1,16 +1,29 @@
-import { Planet } from "~/src/lib/Planet.js"
+import Browser from "webextension-polyfill"
+import { DOMHelpers as D } from "~/src/lib/DOMHelpers.js"
+import { replaceUTCWithLocal } from "~/src/lib/Date.js"
 
-/**
- * Add a button that retrieves resources from extension storage and
- * logs it to console for demonstration purposes.
- */
+const serverTimeElement = D.asHTMLElement(
+    window.document.querySelector("#servertime")
+)
+const serverTimeArray = serverTimeElement.innerHTML
+    .split(":")
+    .map((s) => parseInt(s))
 
-const button = document.createElement("button")
-button.type = "button"
-button.innerText = "Get resources"
-button.onclick = async () => console.log(await Planet.getDepot())
+const localTime = new Date()
+const serverTime = new Date()
+serverTime.setUTCHours(
+    serverTimeArray[0],
+    serverTimeArray[1],
+    serverTimeArray[2]
+)
+const serverTimeDiff = serverTime.getTime() - localTime.getTime()
 
-const td = document.querySelector("#servertime")?.parentElement?.parentElement
-if (td) {
-    td.appendChild(button)
-}
+Browser.storage.local.set({ serverTimeDiff: serverTimeDiff })
+
+replaceUTCWithLocal(serverTimeElement)
+
+serverTimeElement.addEventListener("DOMCharacterDataModified", () => {
+    serverTimeElement.innerHTML = new Date(
+        new Date().getTime() + serverTimeDiff
+    ).toLocaleTimeString()
+})

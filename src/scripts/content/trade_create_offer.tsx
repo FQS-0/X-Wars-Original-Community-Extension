5d82f871032d828b00243b7a1a506f2bb748e360
FQS <oliver@riesewohld.net>
2023-08-02 16:25:35 +0200

    fixed create trade page

--- src/scripts/content/trade_create_offer.tsx
+++ src/scripts/content/trade_create_offer.tsx
@@ -2,139 +2,139 @@ import { createRoot } from "react-dom/client"
 import { as } from "~/src/lib/DOMHelpers.js"
 import { Planet } from "~/src/lib/Planet.js"
 import { Favourite, FavouriteType } from "~src/lib/Favourite.js"
+;(async () => {
+    const kostenMatch = window.document
+        .querySelector(
+            'form[name="formular"] > table > tbody > tr:nth-child(2) > td:nth-child(3)'
+        )
+        ?.textContent?.match(/[\d]+(\.\d+)?/)
+    if (!kostenMatch) throw "Error: transport cost not found"
+    const kosten = parseFloat(kostenMatch[0]) / 100.0
 
-const kostenMatch = window.document
-    .querySelector(
-        'form[name="formular"] > table > tbody > tr:nth-child(2) > td:nth-child(3)'
-    )
-    ?.textContent?.match(/[\d]+(\.\d+)?/)
-if (!kostenMatch) throw "Error: transport cost not found"
-const kosten = parseFloat(kostenMatch[0]) / 100.0
-
-function addReturn() {
-    for (let i = 0; i < 6; i++) {
-        if (
-            parseInt(
-                as(
-                    window.document.forms
-                        .namedItem("formular")
-                        ?.elements.namedItem(`tt_res[${i}]`),
-                    HTMLInputElement
-                ).value
-            ) > 0
-        ) {
-            return
+    function addReturn() {
+        for (let i = 0; i < 6; i++) {
+            if (
+                parseInt(
+                    as(
+                        window.document.forms
+                            .namedItem("formular")
+                            ?.elements.namedItem(`tt_res[${i}]`),
+                        HTMLInputElement
+                    ).value
+                ) > 0
+            ) {
+                return
+            }
         }
+
+        as(
+            window.document.forms
+                .namedItem("formular")
+                ?.elements.namedItem(`tt_res[0]`),
+            HTMLInputElement
+        ).value = "1"
     }
 
-    as(
-        window.document.forms
-            .namedItem("formular")
-            ?.elements.namedItem(`tt_res[0]`),
-        HTMLInputElement
-    ).value = "1"
-}
-
-function addTimeComment() {
-    const date = new Date()
-    const input = as(
-        window.document.forms
-            .namedItem("formular")
-            ?.elements.namedItem("trade_comment"),
-        HTMLInputElement
-    )
-    input.value = input.value.replace(/## \d+:\d+:\d+/, "")
-    input.value += "## " + date.toLocaleTimeString("de-DE")
-}
-
-window.document
-    .querySelector(
-        'form[name="formular"] > table > tbody > tr:last-child > td > a'
-    )
-    ?.addEventListener("click", () => {
-        addReturn()
-        addTimeComment()
-    })
-
-async function limitRes(input: HTMLInputElement, idx: number) {
-    const resources = (await Planet.getDepot()).getCurrentResources()
-    let max_res = Math.trunc(resources.get(idx) / (1 + kosten)) - 2
-    if (max_res < 0) {
-        max_res = 0
+    function addTimeComment() {
+        const date = new Date()
+        const input = as(
+            window.document.forms
+                .namedItem("formular")
+                ?.elements.namedItem("trade_comment"),
+            HTMLInputElement
+        )
+        input.value = input.value.replace(/## \d+:\d+:\d+/, "")
+        input.value += "## " + date.toLocaleTimeString("de-DE")
     }
-    const res = parseInt(input.value)
 
-    if (res > max_res) {
-        input.value = max_res.toString()
-    }
-}
+    window.document
+        .querySelector(
+            'form[name="formular"] > table > tbody > tr:last-child > td > a'
+        )
+        ?.addEventListener("click", () => {
+            addReturn()
+            addTimeComment()
+        })
 
-for (let i = 0; i < 6; i++) {
-    as(
-        window.document.forms
-            .namedItem("formular")
-            ?.elements.namedItem(`tf_res[${i}]`),
-        HTMLElement
-    ).addEventListener("change", function () {
-        limitRes(as(this, HTMLInputElement), i)
-    })
-}
-
-async function save() {
-    const resources = (await Planet.getDepot()).getCurrentResources()
-    for (let i = 0; i < 6; i++) {
-        let max_res = Math.trunc(resources.get(i) / (1 + kosten)) - 2
+    async function limitRes(input: HTMLInputElement, idx: number) {
+        const resources = (await Planet.getDepot()).getCurrentResources()
+        let max_res = Math.trunc(resources.get(idx) / (1 + kosten)) - 2
         if (max_res < 0) {
             max_res = 0
         }
+        const res = parseInt(input.value)
+
+        if (res > max_res) {
+            input.value = max_res.toString()
+        }
+    }
+
+    for (let i = 0; i < 6; i++) {
         as(
             window.document.forms
                 .namedItem("formular")
                 ?.elements.namedItem(`tf_res[${i}]`),
-            HTMLInputElement
-        ).value = max_res.toString()
+            HTMLElement
+        ).addEventListener("change", function () {
+            limitRes(as(this, HTMLInputElement), i)
+        })
     }
 
-    as(
-        window.document.forms
-            .namedItem("formular")
-            ?.elements.namedItem("tt_res[5]"),
-        HTMLInputElement
-    ).value = "9999999"
-    as(
-        window.document.forms
-            .namedItem("formular")
-            ?.elements.namedItem("trade_comment"),
-        HTMLInputElement
-    ).value = "Sicherungshandel"
-}
+    async function save() {
+        const resources = (await Planet.getDepot()).getCurrentResources()
+        for (let i = 0; i < 6; i++) {
+            let max_res = Math.trunc(resources.get(i) / (1 + kosten)) - 2
+            if (max_res < 0) {
+                max_res = 0
+            }
+            as(
+                window.document.forms
+                    .namedItem("formular")
+                    ?.elements.namedItem(`tf_res[${i}]`),
+                HTMLInputElement
+            ).value = max_res.toString()
+        }
 
-const tableRow = as(
-    window.document.querySelector(
-        'form[name="formular"] > table > tbody > tr:last-child'
-    ),
-    HTMLTableRowElement
-)
-const cell = tableRow.insertCell(0)
-cell.colSpan = 2
-cell.className = "first"
-cell.align = "center"
+        as(
+            window.document.forms
+                .namedItem("formular")
+                ?.elements.namedItem("tt_res[5]"),
+            HTMLInputElement
+        ).value = "9999999"
+        as(
+            window.document.forms
+                .namedItem("formular")
+                ?.elements.namedItem("trade_comment"),
+            HTMLInputElement
+        ).value = "Sicherungshandel"
+    }
 
-const newCell = (
-    <>
-        [{" "}
-        <a id="hz__save" href="#" onClick={save}>
-            Sicherungshandel
-        </a>{" "}
-        ]
-    </>
-)
+    const tableRow = as(
+        window.document.querySelector(
+            'form[name="formular"] > table > tbody > tr:last-child'
+        ),
+        HTMLTableRowElement
+    )
+    const cell = tableRow.insertCell(0)
+    cell.colSpan = 2
+    cell.className = "first"
+    cell.align = "center"
 
-const root = createRoot(cell)
-root.render(newCell)
+    const newCell = (
+        <>
+            [{" "}
+            <a id="hz__save" href="#" onClick={save}>
+                Sicherungshandel
+            </a>{" "}
+            ]
+        </>
+    )
 
-/** Add favourites to select */
-;(async () => {
+    const root = createRoot(cell)
+    root.render(newCell)
+
+    /** Add favourites to select */
+
     const select = window.document.querySelector(
         'select[name="changeTarget"]'
     ) as HTMLSelectElement

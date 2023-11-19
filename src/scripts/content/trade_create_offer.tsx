c8beb771df31e1ea2bc7e1587f029a381a3b12c4
FQS <oliver@riesewohld.net>
2023-11-19 10:40:25 +0100

    fixed too many decimal places at transport fee

--- src/scripts/content/trade_create_offer.tsx
+++ src/scripts/content/trade_create_offer.tsx
@@ -691,7 +691,11 @@ const TradeForm = () => {
                 <Grid item xs={12}>
                     <Typography variant="h5">Handelsauftrag</Typography>
                     <Typography variant="subtitle2">
-                        Transportkosten {transportFee * 100}%
+                        Transportkosten{" "}
+                        {new Intl.NumberFormat("de-DE").format(
+                            transportFee * 100
+                        )}
+                        %
                     </Typography>
                 </Grid>
                 <Grid container item spacing={1}>

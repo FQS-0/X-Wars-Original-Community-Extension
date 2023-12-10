b364db345f29f153bdc656c263a3349b0051cfde
FQS <oliver@riesewohld.net>
2023-12-10 15:30:52 +0100

    added button to download member list in json format

--- src/lib/components/sidebar/Memberlist.tsx
+++ src/lib/components/sidebar/Memberlist.tsx
@@ -1,4 +1,5 @@
 import {
+    Button,
     Paper,
     Stack,
     Table,
@@ -10,10 +11,37 @@ import {
     Typography,
 } from "@mui/material"
 import { IAllianceMember } from "~src/lib/json/types/AllianceMember.js"
+import { EFavouriteType } from "~src/lib/json/types/FavouriteType.js"
+import { TFavourites } from "~src/lib/json/types/Favourites.js"
 import { useAllianceMemberList } from "~src/lib/state/AllianceMemberList.js"
 
 export default function MemberList() {
     const memberList = useAllianceMemberList()
+
+    const downloadJson = () => {
+        const coordList: TFavourites = []
+        memberList.forEach((member) =>
+            member.planets.forEach((planet) => {
+                coordList.push({
+                    name: member.name,
+                    coordinates: planet,
+                    type: EFavouriteType.FRIEND,
+                })
+            })
+        )
+        const a = window.document.createElement("a")
+        const file = new Blob(
+            [JSON.stringify({ favourites: coordList }, undefined, 2)],
+            {
+                type: "application/json",
+            }
+        )
+
+        a.href = URL.createObjectURL(file)
+        a.download = "member.json"
+        a.click()
+        URL.revokeObjectURL(a.href)
+    }
 
     return (
         <TableContainer component={Paper}>
@@ -31,6 +59,13 @@ export default function MemberList() {
                     {memberList.map((member) => (
                         <Member member={member} key={member.id} />
                     ))}
+                    <TableRow>
+                        <TableCell colSpan={5}>
+                            <Button onClick={downloadJson}>
+                                Download JSON
+                            </Button>
+                        </TableCell>
+                    </TableRow>
                 </TableBody>
             </Table>
         </TableContainer>

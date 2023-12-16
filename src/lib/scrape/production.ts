7cf1e3eb876c2cd5bec336328be8785bdfd6c49a
FQS <oliver@riesewohld.net>
2023-12-16 09:28:07 +0100

    added freight, tt, lkom and colonization module to ship scraper

--- src/lib/scrape/production.ts
+++ src/lib/scrape/production.ts
@@ -4,6 +4,7 @@ export async function scrapeProduction() {
         const [, id] = (form.name.match(/formular(\d+)/) || ["", "-1"]).map(
             (m) => parseInt(m)
         )
+
         const row = form.nextElementSibling
         if (!(row instanceof HTMLTableRowElement)) {
             console.error("expected table row")
@@ -13,11 +14,13 @@ export async function scrapeProduction() {
             console.error("wrong cell count")
             return
         }
+
         const name = row.cells[0].innerText.replace(/-[^-]+$/, "")
 
         const [, engine, speed] = row.cells[1].innerText.match(
             /(\S{3})\s*(\d+)\s*%/
         ) || [undefined, "UNK", "-1"]
+
         const [, att, def] = (
             row.cells[1].innerText.match(/(\d+)\s*\/\s*(\d+)/) || [
                 "",
@@ -25,6 +28,7 @@ export async function scrapeProduction() {
                 "-1",
             ]
         ).map((m) => parseInt(m))
+
         const [, fe, kr, fr, or, fo, go] = (
             row.cells[1].innerText.match(
                 /([-\d]+)\s*([-\d]+)\s*([-\d]+)\s*([-\d]+)\s*([-\d]+)\s*([-\d]+)\s*$/
@@ -35,6 +39,19 @@ export async function scrapeProduction() {
             return r
         })
 
+        const [, freight] = (
+            row.cells[1].innerText.match(/:\s([.\d]+)/) || ["0", "0"]
+        ).map((m) => parseInt(m.replaceAll(".", "")))
+
+        const components = Array.from(row.cells[1].querySelectorAll("a")).map(
+            (anchor) =>
+                (anchor.href.match(/component_id=(\d+)/) || ["0", "-1"]).map(
+                    (m) => parseInt(m)
+                )[1]
+        )
+
+        console.log(components)
+
         return {
             name: name,
             id: id,
@@ -42,6 +59,13 @@ export async function scrapeProduction() {
             speed: parseInt(speed),
             att: att,
             def: def,
+            freight: freight,
+            tt: components.includes(90),
+            lkom: components.includes(93),
+            colonization: components.includes(91),
+            explorer: components.includes(87),
+            scanner: components.includes(88),
+            observer: components.includes(89),
             resources: {
                 fe: fe,
                 kr: kr,

f1f633e5e58224c515e14538a397cc4503592c1a
FQS <oliver@riesewohld.net>
2023-12-25 14:17:20 +0100

    added shipclass to scraper

--- src/lib/scrape/production.ts
+++ src/lib/scrape/production.ts
@@ -1,6 +1,7 @@
 import { StorageArea } from "../StorageArea.js"
 import { IShip } from "../json/types/Ship.js"
 import { IShipyard } from "../json/types/Shipyard.js"
+import { decode } from "he"
 
 export async function scrapeProduction() {
     const forms = Array.from(window.document.querySelectorAll("form"))
@@ -16,7 +17,41 @@ export async function scrapeProduction() {
             return
         }
 
-        const name = row.cells[0].innerText.replace(/-[^-]+$/, "")
+        const anchor = row.cells[0].querySelector("a")
+        if (!anchor) {
+            console.error("ship link not found!")
+            return
+        }
+        const bi = new URLSearchParams(anchor.href).get("bi")
+        if (!bi) {
+            console.error("bi parameter not found")
+            return
+        }
+        const biArray = atob(bi).split("|")
+        const name = decode(biArray[0].slice(3))
+        console.log(name)
+        const componentsWithCount = biArray
+            .slice(1, -1)
+            .map((encodedComponent) => {
+                const match = encodedComponent.match(/id(\d+)\$(\d+)/)
+                if (!match) return { id: -1, count: -1 }
+                return {
+                    id: parseInt(match[1] || "-2"),
+                    count: parseInt(match[2] || "-2"),
+                }
+            })
+        const components = componentsWithCount.map((comp) => comp.id)
+        const shipClass = components.includes(2)
+            ? "Drohne"
+            : components.includes(3)
+            ? "Taktische Waffe"
+            : components.includes(4)
+            ? "Leicht"
+            : components.includes(5)
+            ? "Mittel"
+            : components.includes(6)
+            ? "Schwer"
+            : "Unbekannt"
 
         const [, engine, speed] = row.cells[1].innerText.match(
             /(\S{3})\s*(\d+)\s*%/
@@ -44,15 +79,8 @@ export async function scrapeProduction() {
             row.cells[1].innerText.match(/:\s([.\d]+)/) || ["0", "0"]
         ).map((m) => parseInt(m.replaceAll(".", "")))
 
-        const components = Array.from(row.cells[1].querySelectorAll("a")).map(
-            (anchor) =>
-                (anchor.href.match(/component_id=(\d+)/) || ["0", "-1"]).map(
-                    (m) => parseInt(m)
-                )[1]
-        )
-
         ships.push({
-            shipClass: "unknown",
+            shipClass: shipClass,
             requirements: [],
             carrier: -1,
             troups: -1,
@@ -81,6 +109,5 @@ export async function scrapeProduction() {
         }
     )
     const shipyard: IShipyard = { name: name, planets: planets, ships: ships }
-
     StorageArea.shipyard.set(shipyard)
 }
